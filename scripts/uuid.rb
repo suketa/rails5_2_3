6e7f655917d6534c4272fa92d67a4f905b2d6bf7
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2019-10-21 09:45:52 +0900

    try106

---
+++ scripts/uuid.rb
@@ -0,0 +1,4 @@
+require 'active_support/core_ext/digest/uuid'
+puts Digest::UUID.uuid_v3(Digest::UUID::DNS_NAMESPACE, 'rubyonrails.org')
+puts Digest::UUID.uuid_v4
+puts Digest::UUID.uuid_v5(Digest::UUID::DNS_NAMESPACE, 'rubyonrails.org')
